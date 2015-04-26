a8a71716ee871072b782c700226bfef2596460f9
Ashvin Kamaraju <akamaraju@akamaraju-win7.vormetric.com>
2015-04-26 09:20:12 -0700

    Working code for caching the inverse of a square matrix. Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## This file implements functions to compute the inverse of a matrix and cache
+## the result. Calculating the inverse of the matrix is computationally
+## intensive (especially for large matrices). Caching the inverse after the
+## initial computation obviates the need to compute it again for the same
+## matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as its input and creates a list of
+## four functions: get (which returns the matrix), set (which stores the
+## matrix), setinv (which stores the inverse of the matrix) and getinv(which
+## returns the stored value of the inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	minv <- NULL
+	get <- function() x
+	set <- function(y) {
+		x <<- y
+		minv <<- NULL
+	}
+	setinv <- function(inverse) minv <<- inverse
+	getinv <- function() minv
+	list (get = get, set = set, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function takes a "square" matrix as its input argument and calculates
+## its inverse. It first checks if the inverse is already cached. If it is not
+## cached it computes the inverse, stores (caches) the min and returns the 
+## inverse. No checks are made to ascertain that the matrix is regular.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	  minv <- x$getinv()
+	  if (!is.null(minv)) {
+		message("getting cached data")
+		return (minv)
+	  }
+	  data <- x$get()
+	  minv <- solve(data,...)
+	  x$setinv(minv)
+	  minv
 }
